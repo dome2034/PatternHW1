f5301ef3bd17e25c21fc302d24360609d5f09787
Thanakit <dome2034@gmail.com>
2018-10-05 00:32:45 +0700

    fiinish

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let fs = require('fs')
+const math = require('mathjs')
 
 let input = fs.readFileSync('TWOCLASS.dat', 'utf8')
 const percentValidate = 10
@@ -35,7 +36,7 @@ const setUpTrainData = (percentValidate, data) => {
 
 const separateClass = (dataSource, classLabel) => {
     return dataSource.filter((data) => { return data[4] === classLabel }).map((val) => {
-        return val.slice(0, -1);
+        return val.slice(0, -1)
     })
 }
 
@@ -45,26 +46,6 @@ const average = datas => {
     }, [0, 0, 0, 0]).map((sum) => {
         return sum / datas.length
     })
-}
-
-const transpose = (array) => {
-    return array[0].map((col, i) => array.map(row => row[i]))
-}
-
-const multiplyMatrix = (a, b) => {
-    let aNumRows = a.length, aNumCols = a[0].length,
-        bNumRows = b.length, bNumCols = b[0].length,
-        m = new Array(aNumRows)
-    for (let r = 0; r < aNumRows; ++r) {
-        m[r] = new Array(bNumCols)
-        for (let c = 0; c < bNumCols; ++c) {
-            m[r][c] = 0
-            for (let i = 0; i < aNumCols; ++i) {
-                m[r][c] += a[r][i] * b[i][c]
-            }
-        }
-    }
-    return m
 }
 
 const multiplyMatrixWithNumber = (datas, num) => {
@@ -79,29 +60,66 @@ const xMinusMean = (datas, means) => {
     })
 }
 
+const fx = (numClass, cov, xMinusMean) => {
+    return (1 / (Math.sqrt(((2 * Math.PI) ^ numClass) * (math.det(cov))))) * math.exp(math.multiply(math.multiply(math.multiply(math.transpose(xMinusMean), -0.5), math.inv(cov)), xMinusMean))
+}
+
 const main = async () => {
     let sourceData = await fetchData.then((value) => {
         value.shift()
         return value
     })
-    console.log('sourceData length:', sourceData.length)
     const testDatas = setUpTestData(percentValidate, sourceData)
     const trainDatas = setUpTrainData(percentValidate, sourceData)
+    let result = []
+    for (i = 0; i <= 9; i++) {
+        let testData = testDatas[i].map((val) => {
+            return val.slice(0, -1)
+        })
 
-    const class1 = separateClass(trainDatas[0], '1')
-    const class2 = separateClass(trainDatas[0], '2')
+        let testClass = testDatas[i].map((val) => {
+            return val.slice(4, 5)
+        })
 
-    const meanClass1 = average(class1)
-    const meanClass2 = average(class2)
+        let trainClass1 = separateClass(trainDatas[i], '1')
+        let trainClass2 = separateClass(trainDatas[i], '2')
 
-    const xMinusMean1 = xMinusMean(class1, meanClass1)
-    const xMinusMean2 = xMinusMean(class2, meanClass2)
+        let meanClass1 = average(trainClass1)
+        let meanClass2 = average(trainClass2)
 
-    const cov1 = multiplyMatrixWithNumber(multiplyMatrix(transpose(xMinusMean1), xMinusMean1), (1 / class1.length))
-    const cov2 = multiplyMatrixWithNumber(multiplyMatrix(transpose(xMinusMean2), xMinusMean2), (1 / class2.length))
-    
-    // console.log(meanClass2)
-    //console.log(class1)
+        let trainXMinusMean1 = xMinusMean(trainClass1, meanClass1)
+        let trainXMinusMean2 = xMinusMean(trainClass2, meanClass2)
+
+        let cov1 = math.multiply(math.multiply(math.transpose(trainXMinusMean1), trainXMinusMean1), (1 / trainClass1.length))
+        let cov2 = math.multiply(math.multiply(math.transpose(trainXMinusMean2), trainXMinusMean2), (1 / trainClass2.length))
+        let a = 0, b = 0, c = 0, d = 0
+        for (j = 0; j <= 9; j++) {
+            let testXMinusMean1 = xMinusMean(testData, meanClass1)
+            let testXMinusMean2 = xMinusMean(testData, meanClass2)
+            let classChoose
+            let fx1 = fx(2, cov1, testXMinusMean1[j])
+            let fx2 = fx(2, cov2, testXMinusMean2[j])
+            if (fx1 === fx2) { classChoose = Math.floor((Math.random() * 2) + 1) }
+            else if (fx1 > fx2) { classChoose = 1 }
+            else if (fx1 < fx2) { classChoose = 2 }
+
+            if (parseInt(testClass[j]) === 1 && classChoose === 1) { a = a + 1 }
+            else if (parseInt(testClass[j]) === 1 && classChoose === 2) { b = b + 1 }
+            else if (parseInt(testClass[j]) === 2 && classChoose === 1) { c = c + 1 }
+            else if (parseInt(testClass[j]) === 2 && classChoose === 2) { d = d + 1 }
+        }
+        console.log(`----------Test ${i+1} --------------`)
+        console.log(`|`, a, b, `|`)
+        console.log(`|`, c, d, `|`)
+        let correct = 100 * (a + d) / (a + b + c + d)
+        let error = 100 - correct
+        console.log(`correct ${correct} %`)
+        console.log(`error ${error} %`)
+        console.log(`-------------------------------\n`)
+    }
+
+    // console.log(fx(2, cov1, xMinusMean1[0]))
+    // console.log(fx(2, cov2, xMinusMean2[0]))
 }
 
 main()
